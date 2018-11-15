f2ffea73b4219a6ce945eeda23c23ffdcebd7e98
Eduardo Costa <costa.ef@gmail.com>
2018-11-15 16:56:50 -0200

    Parse request url

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import http from 'http'
+import url from 'url'
 
 const server = http.createServer((req, res) => {
+  // Get the URL and parse it
+  const parsedUrl = url.parse(req.url, true)
+
+  // Get the path
+  const path = parsedUrl.pathname
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+  // Send the response
   res.end('Hello World\n')
+
+  // Log the request path
+  console.log(`Request received on path: ${trimmedPath}`)
 })
 
 server.listen(3000, () => console.log('The server is listening on port 3000'))
