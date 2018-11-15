fa9da90bde016c71608c69af829baa8e1529c8c1
Eduardo Costa <costa.ef@gmail.com>
2018-11-15 17:31:04 -0200

    Parse request http headers

--- index.js
+++ index.js
@@ -15,11 +15,15 @@ const server = http.createServer((req, res) => {
   // Get the HTTP method
   const method = req.method.toLowerCase()
 
+  // Get the headers as an object
+  const headers = req.headers
+
   // Send the response
   res.end('Hello World\n')
 
   // Log the request path
   console.log(`Request received on path: ${trimmedPath} with method: ${method}, and query params:`, queryStringObject)
+  console.log('Request headers:', headers)
 })
 
 server.listen(3000, () => console.log('The server is listening on port 3000'))
